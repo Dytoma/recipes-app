2843ff63e486589a71edaf6fdba00b400937f014
Dytoma <dytomaomar@gmail.com>
2023-06-11 17:24:09 +0000

    Update the README.md

--- README.md
+++ README.md
@@ -1,6 +1,65 @@
+<h1 align='center'>Recipy App</h1>
+
+
+<h2 align='center'>Discover all the popular meals around and learn how to cook them.</h2>
+
+![Popular meals](https://ibb.co/xSCBqjk)
+
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#description">Description</a></li>
+    <li><a href="#dependencies">Desktop View</a></li>
+    <li><a href="#code-snippets">Mobile View</a></li>
+    <li><a href="#authors">Authors</a></li>
+  </ol>
+</details>
+
+
+## About the project
+
+This is the website where you'll find some popular meals around the world. It has amazing features including **search**, **look for meals by first letter**, **Get random meals**, **View meal details and category details** and much more. The UI iis user friendly and allows you to navigate thorough the website without getting lost. The making of this entire website was possible with [The mealdb API](https://www.themealdb.com/api.php).
+
+#  Features
+<ul>
+<li>Search for specific meals
+<li>See all meals by category
+<li>Get all details about a meal
+<li>Look for meals by first letter
+<li>Get Random meals 
+<li>Toggle between light and dark theme
+</ul>
+
+## Built with:
+- [React](https://legacy.reactjs.org/) - JS library
+- [Next.js 13](https://nextjs.org/) - React Framework
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
+- [Framer Motion](https://www.framer.com/motion/) - For animations
+- [Headless UI](https://headlessui.com/) - For useful UI components
+- [React Icons](https://react-icons.github.io/react-icons) - For icons
+
+
+## Prerequisites
+
+Getting Started
+You need to have [Node js](https://nodejs.org/en/download) installed.
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+## Installation
+
+To install the necessary dependencies `cd recipy-app` and run `npm install` in your terminal.
 
 First, run the development server:
 
@@ -14,9 +73,13 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+You can start editing the pages. The pages auto-update as you edit the files.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+## Code snippets
+Here are some code that I found useful while developing the app:
+
+<img src='./public/images/code.png' align='left' width='420' alt='fetch data' />
+<img src='./public/images/code2.png' align='right' width='400' alt='scroll to top' />
 
 ## Learn More
 
@@ -25,10 +88,11 @@ To learn more about Next.js, take a look at the following resources:
 - [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
 - [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+## Deployed on Vercel
 
-## Deploy on Vercel
+- Live Site URL: [Recipy](https://https://recipes-app-cj7m.vercel.app/)
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+## Authors
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<a href="https://www.linkedin.com/in/dytoma-batogouma-6aa946235/">Dytoma Batogouma</a></br>
+
--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -16,41 +16,41 @@ const Carousel = ({ results }) => {
     const next = () => setCurrent((curr) => (curr === results?.length - 1 ? 0 : curr + 1));
     return (
         <>
-        {details && 
-        
-            <Filter container='details'>
-                <div>
-                    <div className='carousel-container hide'>
-                            <div className='w-full flex items-center smooth_transition' style={{ transform: `translateX(-${current * 100}%)` }}>
-                            {results.map((item, id) => (
-                                <div className='flex flex-col md:flex-row bg-textLight dark:bg-darkRed rounded-2xl items-center gap-5 md:gap-8 lg:gap-10 min-w-full p-5' key={id}>
-                                    <motion.div
-                                        initial={{opacity: 0}}
-                                        whileInView={{opacity: 1}}
-                                        transition={{duration: 0.3, ease: 'easeInOut' }}
-                                    >
-                                        <Image src={item.strCategoryThumb} width={485} height={297} className='w-full h-[200px] md:h-[312px] object-contain lg:object-cover md:min-w-[324px] lg:min-w-[485px] rounded-xl' alt={item.strCategory} />
-                                    </motion.div>
+            {details &&
 
-                                    <motion.div variants={container} initial='hidden' whileInView='show' className='flex flex-col gap-6 p-3 h-[200px] md:h-[312px] overflow-auto'>
-                                        <motion.h3 variants={itemFade} whileInView={{ opacity: [0, 1] }} className='text-xl font-barlow tracking-wider text-[#1E1E1E] font-semibold dark:text-white'>{item?.strCategory}</motion.h3>
-                                        <motion.p variants={itemFade} whileInView={{ opacity: [0, 1] }} className='text-base tracking-wider dark:text-textLight font-poppins'>{item?.strCategoryDescription}</motion.p>
-                                    </motion.div>
-                                </div>
-                            ))}
+                <Filter container='details'>
+                    <div>
+                        <div className='carousel-container hide'>
+                            <div className='w-full flex items-center smooth_transition' style={{ transform: `translateX(-${current * 100}%)` }}>
+                                {results.map((item, id) => (
+                                    <div className='flex flex-col md:flex-row bg-textLight dark:bg-darkRed rounded-2xl items-center gap-5 md:gap-8 lg:gap-10 min-w-full p-5' key={id}>
+                                        <motion.div
+                                            initial={{ opacity: 0 }}
+                                            whileInView={{ opacity: 1 }}
+                                            transition={{ duration: 0.3, ease: 'easeInOut' }}
+                                        >
+                                            <Image src={item.strCategoryThumb} width={485} height={297} className='w-full h-[200px] md:h-[312px] object-contain lg:object-cover md:min-w-[324px] lg:min-w-[485px] rounded-xl' alt={item.strCategory} />
+                                        </motion.div>
+
+                                        <motion.div variants={container} initial='hidden' whileInView='show' className='flex flex-col gap-6 p-3 h-[200px] md:h-[312px] overflow-auto'>
+                                            <motion.h3 variants={itemFade} whileInView={{ opacity: [0, 1] }} className='text-xl font-barlow tracking-wider text-[#1E1E1E] font-semibold dark:text-white'>{item?.strCategory}</motion.h3>
+                                            <motion.p variants={itemFade} whileInView={{ opacity: [0, 1] }} className='text-base tracking-wider dark:text-textLight font-poppins'>{item?.strCategoryDescription}</motion.p>
+                                        </motion.div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-center gap-5 mt-6 md:mt-8 lg:mt-10'>
+                            <button type='button' className='p-2 rounded-full bg-white dark:bg-darkRed dark:text-textLight smooth_transition hover:opacity-50'>
+                                <BiChevronLeft className='text-xl text-textColor dark:text-textLight' onClick={prev} />
+                            </button>
+                            <button type='button' className='p-2 rounded-full bg-white dark:bg-darkRed dark:text-textLight smooth_transition hover:opacity-50'>
+                                <BiChevronRight className='text-xl text-textColor dark:text-textLight' onClick={next} />
+                            </button>
                         </div>
                     </div>
-                    <div className='flex items-center justify-center gap-5 mt-6 md:mt-8 lg:mt-10'>
-                            <button type='button' className='p-2 rounded-full bg-white dark:bg-darkRed dark:text-textLight smooth_transition hover:opacity-50'>
-                            <BiChevronLeft className='text-xl text-textColor dark:text-textLight' onClick={prev} />
-                        </button>
-                            <button type='button' className='p-2 rounded-full bg-white dark:bg-darkRed dark:text-textLight smooth_transition hover:opacity-50'>
-                            <BiChevronRight className='text-xl text-textColor dark:text-textLight' onClick={next} />
-                        </button>
-                    </div>
-                </div>
-            </Filter>
-        }
+                </Filter>
+            }
         </>
     )
 }
